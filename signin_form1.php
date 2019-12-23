830fecb392e68c54469e6a9eee37d9d2a2e49c03
Chetan Pareek <52780835+cp99says@users.noreply.github.com>
2019-12-24 00:22:19 +0530

    ajax
    ajax not gone right

--- signin_form1.php
+++ signin_form1.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="refresh" content="" >
+<head>	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style2.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	
 	<title>StreamX-Login</title>
 </head>
 <style type="text/css">
@@ -12,12 +13,12 @@
 <body>
 	<span style="font-size: 2em">Login page</span>
 	<div class="form1">
-       <form action="get1.php" method="post" id="myform" >
+       <form action="" method="post" id="myform" >
     	<div class="inputbox">
     		<label >Username</label>
     		<input type="text" name="username" class="input">    		
     	</div><br><br>
-    	<div class="inputbox">
+    	<div class="inputbox"> 
     		<label>Password</label>
     		<input name="password" type="password" class="input">	
      	 </div>		<br><br>
@@ -28,23 +29,37 @@
             </div>
         </form>
     </div>
+
+
+
     <script type="text/javascript">
-    	$(document).ready(function(){
-             var form=$('#myform');
-			 $('submit').click(function(){
-                    $.ajax({
-                       url: form.attr("action");
-                      type: 'post',
-					  data:$("#myform :input").serialize(),
+        $(document).ready(function()
+        {   require'connect.php';
+            $('#submit').click(function()
+            {     return 0;
+                 if( function() == 0 )
+                {
 
-
-                       success: function(data)
-					   {
-						   console.log(data);
-					   }
-					});
-			 });
-		});
+                      $user=$_POST['username'];
+                      $pwd=$_POST['password'];  
+                      $sql="SELECT * FROM STUDENT_INFO WHERE username='$user' && password='$pwd' ";
+                      $data=mysqli_query($con,$sql);
+                      $total=mysqli_num_rows($data);
+                      if($total==1)
+                      {
+                        echo 'password correct';
+                      }
+                      else
+                        echo 'incorrect password';
+                } 
+                function(data,status)
+               {
+                   $('#myform').html(data);
+                   alert(status);
+               }; 
+            });
+        });
+        
     </script>
 </body>
 </html>     
